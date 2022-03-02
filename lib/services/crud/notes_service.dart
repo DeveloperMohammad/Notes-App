64c0899ccebc9f65728d6d0cf974b797cd255212
DeveloperMohammad <linuxfan576@gmail.com>
2022-03-02 09:17:47 +0430

    step-18

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
+import 'package:notes/extensions/list/filter.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart' show join;
@@ -12,28 +13,45 @@ class NotesService {
 
   List<DatabaseNote> _notes = [];
 
+  DatabaseUser? _user;
+
   static final NotesService _shared = NotesService._sharedInstance();
   NotesService._sharedInstance() {
-    _notesStreamController = StreamController<List<DatabaseNote>>.broadcast(
-      onListen: () {
-        _notesStreamController.sink.add(_notes);
-      }
-    );
+    _notesStreamController =
+        StreamController<List<DatabaseNote>>.broadcast(onListen: () {
+      _notesStreamController.sink.add(_notes);
+    });
   }
   factory NotesService() => _shared;
 
   late final StreamController<List<DatabaseNote>> _notesStreamController;
 
-  Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream;
+  Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream.filter((note) {
+      final currentUser = _user;
+      if(currentUser != null) {
+        return note.userId == currentUser.id;
+      } else {
+        throw UserShouldBeSetBeforeReadingAllNotes();
+      }
+    });
 
-  Future<DatabaseUser> getOrCreateUser({required String email}) async {
+  Future<DatabaseUser> getOrCreateUser({
+    required String email,
+    bool setAsCurrentUser = true,
+  }) async {
     try {
-    final user = await getUser(email: email);
-    return user;
+      final user = await getUser(email: email);
+      if(setAsCurrentUser) {
+        _user = user;
+      }
+      return user;
     } on CouldNotFindUser {
       final createdUser = await createUser(email: email);
+      if(setAsCurrentUser) {
+        _user = createdUser;
+      }
       return createdUser;
-    } catch(e) {
+    } catch (e) {
       rethrow;
     }
   }
@@ -44,7 +62,10 @@ class NotesService {
     _notesStreamController.add(_notes);
   }
 
-  Future<DatabaseNote> updateNote({required DatabaseNote note, required String text}) async {
+  Future<DatabaseNote> updateNote({
+    required DatabaseNote note,
+    required String text,
+  }) async {
     await _ensureDbIsOpen();
     final db = _getDatabaseOrThrow();
 
@@ -52,10 +73,15 @@ class NotesService {
     await getNote(id: note.id);
 
     // update DB
-    final updateCount = await db.update(noteTable, {
-      textColumn: text,
-      isSyncedWithCloudColumn: 0,
-    });
+    final updateCount = await db.update(
+      noteTable,
+      {
+        textColumn: text,
+        isSyncedWithCloudColumn: 0,
+      },
+      where: 'id = ?',
+      whereArgs: [note.id],
+    );
 
     if (updateCount == 0) {
       throw CouldNotUpdateNote();
@@ -69,11 +95,12 @@ class NotesService {
   }
 
   Future<Iterable<DatabaseNote>> getUserNotes({required String email}) async {
-  final db = _getDatabaseOrThrow();
-  final notes = await db.query(noteTable, where: '$email = ?', whereArgs: [email]);
+    final db = _getDatabaseOrThrow();
+    final notes =
+        await db.query(noteTable, where: '$email = ?', whereArgs: [email]);
 
-  return notes.map((note) => DatabaseNote.fromRow(note));
-}
+    return notes.map((note) => DatabaseNote.fromRow(note));
+  }
 
   Future<Iterable<DatabaseNote>> getAllNotes() async {
     await _ensureDbIsOpen();
@@ -299,7 +326,8 @@ class DatabaseNote {
       : id = map[idColumn] as int,
         userId = map[userIdColumn] as int,
         text = map[textColumn] as String,
-        isSyncedWithCloud = (map[isSyncedWithCloudColumn] as int) == 1 ? true : false;
+        isSyncedWithCloud =
+            (map[isSyncedWithCloudColumn] as int) == 1 ? true : false;
 
   @override
   String toString() =>
